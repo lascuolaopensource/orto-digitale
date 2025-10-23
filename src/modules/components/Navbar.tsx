04d325b0d31081509e233d61272871c0308fafb2
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-10-23 15:48:39 +0200

    Update Navbar.tsx

--- src/modules/components/Navbar.tsx
+++ src/modules/components/Navbar.tsx
@@ -1,14 +1,20 @@
+import { getRandomRotationClass } from '#/utils'
+import { cn } from '$/lib/utils'
 import Link from 'next/link'
+
+//
 
 export function Navbar() {
 	return (
 		<div>
-			<div className="max-w-7xl mx-auto px-6 py-2">
-				<NavbarLink href="/">Home</NavbarLink>
-				<NavbarLink href="/piante">Piante</NavbarLink>
-				<NavbarLink href="/scopri">Scopri</NavbarLink>
-				<NavbarLink href="/ricette">Ricette</NavbarLink>
-				<NavbarLink href="/about">About</NavbarLink>
+			<div className="max-w-7xl mx-auto px-6 py-2 flex items-center justify-between">
+				<NavbarLink href="/">🏠 Home</NavbarLink>
+				<div className="flex -space-x-0.5">
+					<NavbarLink href="/piante">🌱 Piante</NavbarLink>
+					<NavbarLink href="/scopri">🍱 Scopri l'orto</NavbarLink>
+					<NavbarLink href="/ricette">🍽️ Ricette</NavbarLink>
+					<NavbarLink href="/about">👤 About</NavbarLink>
+				</div>
 			</div>
 		</div>
 	)
@@ -26,7 +32,12 @@ function NavbarLink(props: NavbarLinkProps) {
 	return (
 		<Link
 			href={href}
-			className="text-sm font-medium text-primary hover:text-primary/80 hover:underline"
+			className={cn(
+				'border border-red-500 p-2 rounded-lg block w-fit bg-card',
+				'text-sm font-medium text-primary hover:text-primary/80',
+				'transition-transform hover:bg-red-400 hover:text-white hover:z-10',
+				getRandomRotationClass('md'),
+			)}
 		>
 			{children}
 		</Link>
