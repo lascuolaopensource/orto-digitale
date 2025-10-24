20d6c0b38e55ccfd8ba860de75bed8efd9a66bf4
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-10-24 12:05:06 +0200

    improve map

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,28 @@
+import { ArrowBigDown } from 'lucide-react'
+
+import { BoxedHeading } from '@/modules/components/boxed-heading'
 import Giardino from '@/modules/components/giardino'
+import { T } from '@/modules/components/t'
 
 export default async function HomePage() {
-	return <Giardino />
+	return (
+		<div className="flex flex-row-reverse w-full overflow-x-scroll p-6">
+			<Giardino className="h-[calc(100vh-86px)]">
+				<div className="flex items-center gap-8 pr-6">
+					<ArrowBigDown size={40} className="animate-bounce rotate-90" />
+					<div className="flex flex-col items-end">
+						<BoxedHeading tag="h2" className="w-fit mb-2 translate-x-2">
+							Scopri l'orto!
+						</BoxedHeading>
+						<T className="max-w-[300px] text-balance text-right">
+							Clicca su un'area colorata
+							<br /> per scoprire di più!
+							<br />
+							<span className="text-sm italic">(Se serve, scrolla a sinistra)</span>
+						</T>
+					</div>
+				</div>
+			</Giardino>
+		</div>
+	)
 }
--- src/modules/components/Navbar.tsx
+++ src/modules/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import { getRandomRotationClass } from '#/utils'
+import { Button } from '$/components/ui/button'
 import { cn } from '$/lib/utils'
+import { MenuIcon } from 'lucide-react'
 import Link from 'next/link'
 
 //
@@ -9,12 +11,18 @@ export function Navbar() {
 		<div>
 			<div className="max-w-7xl mx-auto px-6 py-2 flex items-center justify-between">
 				<NavbarLink href="/">🏠 Home</NavbarLink>
-				<div className="flex -space-x-0.5">
+
+				<div className="-space-x-0.5 hidden sm:flex">
 					<NavbarLink href="/piante">🌱 Piante</NavbarLink>
 					<NavbarLink href="/scopri">🍱 Scopri l'orto</NavbarLink>
 					<NavbarLink href="/ricette">🍽️ Ricette</NavbarLink>
 					<NavbarLink href="/about">👤 About</NavbarLink>
 				</div>
+
+				<Button className={cn(buttonClasses(), 'sm:hidden flex items-center cursor-pointer')}>
+					<MenuIcon size={24} />
+					<span>Menu</span>
+				</Button>
 			</div>
 		</div>
 	)
@@ -27,18 +35,19 @@ type NavbarLinkProps = {
 	children: React.ReactNode
 }
 
+function buttonClasses() {
+	return cn(
+		'border border-red-500 p-2 rounded-lg block w-fit bg-card',
+		'text-sm font-medium text-primary hover:text-primary/80',
+		'transition-transform hover:bg-red-400 hover:text-white hover:z-10',
+		getRandomRotationClass('md'),
+	)
+}
+
 function NavbarLink(props: NavbarLinkProps) {
 	const { href, children } = props
 	return (
-		<Link
-			href={href}
-			className={cn(
-				'border border-red-500 p-2 rounded-lg block w-fit bg-card',
-				'text-sm font-medium text-primary hover:text-primary/80',
-				'transition-transform hover:bg-red-400 hover:text-white hover:z-10',
-				getRandomRotationClass('md'),
-			)}
-		>
+		<Link href={href} className={buttonClasses()}>
 			{children}
 		</Link>
 	)
