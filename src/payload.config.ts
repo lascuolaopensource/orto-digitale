7ca10a6d80db4fa3366454a389a6bb92a808129d
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-11-01 10:47:14 +0100

    setup db and s3

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,7 +1,9 @@
 // storage-adapter-import-placeholder
 
+import { postgresAdapter } from '@payloadcms/db-postgres'
 import { sqliteAdapter } from '@payloadcms/db-sqlite'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { s3Storage } from '@payloadcms/storage-s3'
 import { it } from '@payloadcms/translations/languages/it'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -25,16 +27,8 @@ export default buildConfig({
 	collections: [Users, Media, Areas, Plants, Recipes],
 	globals: [About, Meta],
 
-	plugins: [
-		// payloadCloudPlugin(),
-		// storage-adapter-placeholder
-	],
-
-	db: sqliteAdapter({
-		client: {
-			url: process.env.DATABASE_URI || '',
-		},
-	}),
+	db: db(),
+	plugins: [s3()],
 
 	admin: {
 		user: Users.slug,
@@ -53,3 +47,45 @@ export default buildConfig({
 		outputFile: path.resolve(dirname, 'payload-types.ts'),
 	},
 })
+
+function env<T>(props: { prod: T; dev: T }) {
+	return process.env.NODE_ENV === 'production' ? props.prod : props.dev
+}
+
+function db() {
+	return env({
+		prod: postgresAdapter({
+			pool: {
+				connectionString: process.env.DATABASE_URI || '',
+			},
+		}),
+		dev: sqliteAdapter({
+			client: {
+				url: process.env.DATABASE_URI || '',
+			},
+		}),
+	})
+}
+
+function s3() {
+	return s3Storage({
+		disableLocalStorage: true,
+		collections: {
+			[Media.slug]: {
+				disableLocalStorage: true,
+				prefix: 'media',
+			},
+		},
+		bucket: process.env.S3_BUCKET!,
+		enabled: process.env.NODE_ENV === 'production',
+		config: {
+			endpoint: process.env.S3_ENDPOINT!,
+			region: process.env.S3_REGION!,
+			credentials: {
+				accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+				secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+			},
+			forcePathStyle: true,
+		},
+	})
+}
